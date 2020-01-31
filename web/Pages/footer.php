a6cd485a4b6ca9207ac7b018ef001a1363438e05
Arthur <dolcolost199@gmail.com>
2020-01-31 01:17:32 +0100

    Add web directory
    Add some web pages from Cédric and modify css and index.php

---
+++ web/Pages/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div id="table_footer" class="container">
+        <div class="row">
+            <a href="#" class="col link_footer">Nos services</a>
+            <a href="#" class="col link_footer">Reservation de prestation</a>
+        </div>
+        <div class="row">
+            <a href="#" class="col link_footer">Information Tarif</a>
+            <a href="#" class="col link_footer">Nous contacter</a>
+        </div>
+        <div class="row">
+            <a href="#" class="col link_footer">Se connecter</a>
+            <a href="#" class="col link_footer">S'inscrire</a>
+        </div>
+    </div>
+    <br/>
+    <br/>
+    <span>©GARVENES Cédric, CHAMPION Cyrille et BRONGNIART Arthur 2020</span>
+</footer>
---
+++ web/Pages/header.php
@@ -0,0 +1,36 @@
+<header>
+    <h1>LuxeryService</h1>
+    <nav>
+        <ul id="menu_barre">
+            <li class="link_header">
+                <a href="index.php"><img src="Pictures/Logo_LuxeryService.png" alt="Image LuxeryService" height="80px"/></a>
+            </li>
+            <li class="link_header">
+                <a href="index.php" class="link">Home Page</a>&nbsp&nbsp&nbsp|
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">Nos Services</a>&nbsp&nbsp&nbsp|
+                <ul class="under_menu">
+                    <li><a href="#" class="link_menu">Services simples</a></li>
+                    <li><a href="#" class="link_menu">Services récurents</a></li>
+                    <li><a href="#" class="link_menu">Abonnements</a></li>
+                </ul>
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">Réservation de préstation</a>&nbsp&nbsp&nbsp|
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">Informations Tarifs</a>&nbsp&nbsp&nbsp|
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">Nous contacter</a>&nbsp&nbsp&nbsp|
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">Se connecter</a>&nbsp&nbsp&nbsp|
+            </li>
+            <li class="link_header">
+                <a href="#" class="link">S'inscrire</a>
+            </li>
+        <ul>
+    </nav>
+</header>
