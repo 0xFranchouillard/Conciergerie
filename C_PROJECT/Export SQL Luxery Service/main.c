743356174f9e8fe308f9b2a3dce9fdd2d4f5c81a
Cédric GARVENES <cedricgarvenes@hotmail.fr>
2020-03-21 18:45:28 +0100

    MaJ Export SQL (add free + Mysql Localization and Export storage folder)

--- C_PROJECT/Export SQL Luxery Service/main.c
+++ C_PROJECT/Export SQL Luxery Service/main.c
@@ -4,17 +4,13 @@
 
 int main(int argc,char **argv)
 {
-    char *verifServer;
-    char *verifUser;
-    char *verifPassword;
-    char *verifDataBase;
-    char *verifPort;
     char *server = malloc(sizeof(char)*65);
     char *user = malloc(sizeof(char)*65);
     char *password = malloc(sizeof(char)*65);
     char *dataBase = malloc(sizeof(char)*65);
-    //char *port = malloc(sizeof(char)*5);
     int port;
+    char *mysqlLocalization = malloc(sizeof(char)*129);
+    char *exportStorage = malloc(sizeof(char)*129);
     char *exportDB = malloc(sizeof(char)*326);
     FILE *file = fopen("Configuration.txt", "r");
     if(file == NULL) {
@@ -23,20 +19,15 @@ int main(int argc,char **argv)
     }
     char line[64];
     while(fgets(line, sizeof(line), file) != NULL) {
-        verifServer = strstr(line, "Server");
-        verifUser = strstr(line, "User");
-        verifPassword = strstr(line, "Password");
-        verifDataBase = strstr(line, "Data Base");
-        verifPort = strstr(line, "Port");
-        if(verifServer != NULL) {
+        if(strstr(line, "Server") != NULL) {
             strncpy(server, line+9, strlen(line+9)-1);
             server[strlen(line+9)-1]='\0';
         }
-        if(verifUser != NULL) {
+        if(strstr(line, "User") != NULL) {
             strncpy(user, line+7, strlen(line+7)-1);
             user[strlen(line+7)-1]='\0';
         }
-        if(verifPassword != NULL) {
+        if(strstr(line, "Password") != NULL) {
             if(strlen(line+11)-1 != 0){
                 strcpy(password,"-p");
                 strncat(password, line+11, strlen(line+11)-1);
@@ -46,21 +37,33 @@ int main(int argc,char **argv)
                 password[strlen(line+11)-1]='\0';
             }
         }
-        if(verifDataBase != NULL) {
+        if(strstr(line, "Data Base") != NULL) {
             strncpy(dataBase, line+12, strlen(line+12)-1);
             dataBase[strlen(line+12)-1]='\0';
         }
-        if(verifPort != NULL) {
+        if(strstr(line, "Port") != NULL) {
             sscanf(line+7,"%d",&port);
-            //strncpy(port, line+7, strlen(line+7));
-            //port[strlen(line+7)]='\0';
+        }
+        if(strstr(line, "MySQL Localization") != NULL) {
+            strncpy(mysqlLocalization, line+21, strlen(line+21)-1);
+            mysqlLocalization[strlen(line+21)-1]='\0';
+        }
+        if(strstr(line, "Export storage folder") != NULL) {
+            strncpy(exportStorage, line+24, strlen(line+24));
+            exportStorage[strlen(line+24)]='\0';
         }
     }
 
-    sprintf(exportDB,"D: & cd D:\\Wamp\\bin\\mysql\\mysql8.0.18\\bin && mysqldump --skip-triggers --compact --no-create-info --extended-insert=false -P %d -h %s -u %s %s %s serviceprovider > C:\\Users\\User\\Desktop\\export.sql", port, server, user, password, dataBase);
-    //printf("%s\n",exportDB);
+    sprintf(exportDB,"D: & cd %s && mysqldump --skip-triggers --compact --no-create-info --extended-insert=false -P %d -h %s -u %s %s %s serviceprovider > %s\\export_%s.sql", mysqlLocalization, port, server, user, password, dataBase, exportStorage, dataBase);
+    printf("%s\n",exportDB);
 
     system(exportDB);
+
+    free(server);
+    free(user);
+    free(password);
+    free(dataBase);
+    free(exportDB);
 
     return 0;
 }
--- C_PROJECT/Fusion SQL Luxery Service/main.c
+++ C_PROJECT/Fusion SQL Luxery Service/main.c
@@ -1,51 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
+
+typedef struct dirent dirent;
+
+void readFile(const char *nameFile);
 
 int main(int argc,char **argv)
 {
-    char *verif_user;
-    char *verif_password;
-    char *verif_db;
-    char *user = malloc(sizeof(char)*128+1);
-    char *password = malloc(sizeof(char)*128+1);
-    char *db = malloc(sizeof(char)*128+1);
-    char *export_db = malloc(sizeof(char)*255+1);
-    static const char filename[] = "Configuration.txt";
-    FILE *file = fopen ( filename, "r" );
-    if ( file != NULL ) {
-      char line [ 128 ];
-      while ( fgets ( line, sizeof line, file ) != NULL ) {
-        verif_db = strstr(line, "Data Base");
-        verif_user = strstr(line, "User");
-        verif_password = strstr(line, "Password");
-        if(verif_db != NULL) {
-            strncpy(db,line+12,strlen(line+12)-1);
-            db[strlen(line+12)-1]='\0';
-        }
-        if(verif_user != NULL) {
-            strncpy(user,line+7,strlen(line+7)-1);
-            user[strlen(line+7)-1]='\0';
-        }
-        if(verif_password != NULL) {
-            strncpy(password,line+11,strlen(line+11)-1);
-            password[strlen(line+11)-1]='\0';
-        }
+    DIR* rep = NULL;
+    dirent* file = NULL; /* Déclaration d'un pointeur vers la structure dirent. */
+    rep = opendir("D:\\ESGI\\ExportPA"); /* Ouverture d'un dossier */
 
-      }
-        sprintf(export_db,"D: & cd D:\\Wamp\\bin\\mysql\\mysql8.0.18\\bin && mysqldump --no-create-info --skip-triggers --skip-add-drop-table --skip-add-locks --skip-disable-keys  --insert-ignore --extended-insert=false -u %s %s > C:\\Users\\User\\Desktop\\export.sql",user,db);
-        printf("%s", export_db);
-        fclose ( file );
-    } else {
-        perror ( filename );
+    if (rep == NULL) { /* Si le dossier n'a pas pu être ouvert */
+        perror(""); /* perror() écrit l'erreur */
+        exit(0);
     }
-    system(export_db);
-    system("cd D:\\Wamp\\bin\\mysql\\mysql8.0.18\\bin && mysql -u root luxeryservice_sauvegarde < C:\\Users\\User\\Desktop\\export.sql");
 
-    free(user);
-    free(password);
-    free(db);
-    free(export_db);
+    while ((file = readdir(rep)) != NULL) { /* On lit le répertoire du dossier */
+        if (strstr(file->d_name,"export_luxeryservice") != NULL && strstr(file->d_name,".sql") != NULL) {
+            readFile(file->d_name);
+        }
+    }
+
+    if (closedir(rep) == -1) { /* S'il y a eu un souci avec la fermeture */
+        perror(""); /* perror() écrit l'erreur */
+        exit(0);
+    }
 
     return 0;
+}
+
+void readFile(const char *nameFile) {
+
+    FILE *file = NULL;
+    char *nameFileFull = malloc(sizeof(char)*256);
+    char *insertSQL = malloc(sizeof(char)*256);
+    if (nameFileFull == NULL && insertSQL == NULL) {
+        printf("Allocation error");
+        exit(0);
+    }
+
+    strcpy(nameFileFull,"D:\\ESGI\\ExportPA\\");
+    strcat(nameFileFull,nameFile);
+
+    file = fopen(nameFileFull, "r");
+    if (file == NULL) {
+        perror("");
+        exit(0);
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if(strstr(line,"INSERT INTO") != NULL) {
+            strncpy(insertSQL, line, strlen(line)-1);
+            insertSQL[strlen(line)-1]='\0';
+            printf("%s\n",insertSQL);
+        }
+    }
+
 }
