1f154a58b98ce7053aec541137c736b822cc4307
CyrilleChampion <cyrillechampion13@gmail.com>
2020-04-22 18:03:00 +0200

    comm

--- web/connection.php
+++ web/connection.php
@@ -14,7 +14,7 @@ if($connected==true){
 		<meta charset="utf-8">
 		<meta name="description" content="Projet Annuel">
 		<link rel="stylesheet" type="text/css" href="CSS/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="CSS/CSS_Luxery.css">
+		<link rel="stylesheet" type="text/css" href="CSS/CSS_luxery.css">
 		<title>LuxeryService</title>
 	</head>
 	<body>
